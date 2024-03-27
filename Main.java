d4015b10dd428b1607b8a0edf9b6bf3e0b5b99a8
cakeller2 <cakeller2@quinnipiac.edu>
2024-03-27 19:39:26 -0400

    ascii welcome message

--- Main.java
+++ Main.java
@@ -16,8 +16,24 @@ public class Main {
 
     
     public static void main(String[] args) {
-        System.out.println("Welcome to qBay!");
+        System.out.println("\r\n" + //
+                        " _    _        _                                 _                  ______                \r\n" + //
+                        "| |  | |      | |                               | |                 | ___ \\               \r\n" + //
+                        "| |  | |  ___ | |  ___   ___   _ __ ___    ___  | |_   ___     __ _ | |_/ /  __ _  _   _  \r\n" + //
+                        "| |/\\| | / _ \\| | / __| / _ \\ | '_ ` _ \\  / _ \\ | __| / _ \\   / _` || ___ \\ / _` || | | | \r\n" + //
+                        "\\  /\\  /|  __/| || (__ | (_) || | | | | ||  __/ | |_ | (_) | | (_| || |_/ /| (_| || |_| | \r\n" + //
+                        " \\/  \\/  \\___||_| \\___| \\___/ |_| |_| |_| \\___|  \\__| \\___/   \\__, |\\____/  \\__,_| \\__, | \r\n" + //
+                        "                                                                 | |                __/ | \r\n" + //
+                        "                                                                 |_|               |___/  \r\n" + //
+                        "");                                                                                                             
         System.out.println("*Insert Selection Message Here*");
     }
     
 }
+
+/*This POC app should first welcome the user with a fun welcome message with associated ASCII art
+(websites like the one here can help you with this), and then ask a student user to login using their
+Quinnipiac email and password. You do not need to setup a real-world login system – just have a couple
+of accounts hardcoded that can be used. If the student fails to login three times due to incorrect
+username/password, tell the user that they are locked out of their account due to suspicious activity and
+force exit the application.*/
