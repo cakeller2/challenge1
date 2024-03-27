c2188c5d4a1a7c13a4ef50801151dbfe06eb5b66
Requiem493 <akshaj.illa@gmail.com>
2024-03-27 19:12:24 -0400

    made Main class

---
+++ Main.java
@@ -0,0 +1,23 @@
+package challenge1;
+
+public class Main {
+
+    public void Buy(){
+
+    }
+
+    public void Sell(){
+
+    }
+
+    public void viewCart(){
+
+    }
+
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to qBay!");
+        System.out.println("*Insert Selection Message Here*");
+    }
+    
+}
