37fbf9ca163b367929987178fc6f34719e935c9b
Requiem493 <akshaj.illa@gmail.com>
2024-03-27 19:07:47 -0400

    Added Item class and constructor.

---
+++ Item.java
@@ -0,0 +1,19 @@
+package challenge1;
+
+public class Item {
+    
+    String itemName;
+    String category;
+    String name;
+    String email;
+    double price;
+
+    public Item(String itemName, String category, String name, String email, double price){
+
+        this.itemName = itemName;
+        this.category = category;
+        this.name = name;
+        this.email = email;
+        this.price = price;
+    }
+}
