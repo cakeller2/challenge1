ccced90c9205381a4e212f927ca12959176000fc
cakeller2 <cakeller2@quinnipiac.edu>
2024-04-10 00:22:58 -0400

    getting rid of unneeded comments

--- Main.java
+++ Main.java
@@ -151,10 +151,3 @@ public class Main {
     }
     
 }
-
-/*This POC app should first welcome the user with a fun welcome message with associated ASCII art
-(websites like the one here can help you with this), and then ask a student user to login using their
-Quinnipiac email and password. You do not need to setup a real-world login system – just have a couple
-of accounts hardcoded that can be used. If the student fails to login three times due to incorrect
-username/password, tell the user that they are locked out of their account due to suspicious activity and
-force exit the application.*/
